6257ab534a4b8e50be136a2e682b9feca97518c6
iolsilva <122048176+iolsilva@users.noreply.github.com>
2023-02-07 14:19:52 -0300

    Add files via upload
    Update

--- script.js
+++ script.js
@@ -31,3 +31,17 @@ function save () {
 const data = JSON.parse(localStorage.getItem("NLWSetup@habits")) || {}
 nlwSetup.setData(data)
 nlwSetup.load()
+
+const header = document.getElementById('header')
+ const headerClassList = header.classList
+ window.addEventListener('scroll', () => {
+   if (window.scrollY >= 100) {
+     if (!headerClassList.contains('scrollHide')) {
+       headerClassList.add('scrollHide')
+     }
+   } else {
+     if (headerClassList.contains('scrollHide')) {
+       headerClassList.remove('scrollHide')
+     }
+   }
+ })
